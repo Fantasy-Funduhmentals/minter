54f428b760a3a46e669d6ce561cd08daac126f82
Fasih <fasih.haider@kryptomind.com>
2022-11-23 18:14:37 +0500

    Fix cli binary path

--- cardano.js
+++ cardano.js
@@ -3,6 +3,7 @@ const CardanocliJs = require("cardanocli-js");
 const cardano = new CardanocliJs({
   network: "testnet-magic 1",
   shelleyGenesisPath: "/var/www/Cardano/config/shelley-genesis.json",
+  cliPath: "/var/www/Cardano/binary",
   socketPath: "/var/www/Cardano/node.socket"
 });
 
