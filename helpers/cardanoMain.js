668eb3b86d73aa96b11dce48072f45acaf094de3
Fasih <fasih.haider@kryptomind.com>
2023-03-30 16:52:15 +0500

    Add start script

--- helpers/cardanoMain.js
+++ helpers/cardanoMain.js
@@ -1,7 +1,7 @@
 const CardanocliJs = require("cardanocli-js");
 
 const cardano = new CardanocliJs({
-  network: "mainnet-magic 764824073",
+  network: "mainnet",
   shelleyGenesisPath: "../config/shelley-genesis.json",
   cliPath: "../binary/cardano-cli",
   socketPath: "../node.socket",
